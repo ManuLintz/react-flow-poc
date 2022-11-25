572e768e1517af20a36158506e06e713d653ecc5
Manuel Lintz <manuel.lintz@gmail.com>
2022-11-25 15:21:46 +0100

    first commit

---
+++ src/App/index.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import Flow from '../Flow';
+import Button from '@mui/material/Button';
+import Drawer from '@mui/material/Drawer';
+
+import './App.css';
+
+function App() {
+  const [state, setState] = React.useState({
+    isDrawerOpen: false,
+  });
+
+  const toggleDrawer =
+    () =>
+    (event: React.KeyboardEvent | React.MouseEvent) => {
+      setState({ ...state, isDrawerOpen: !state.isDrawerOpen });
+    };
+
+  return (
+    <div className="App">
+      <header className="App-header">React Flow - CRA Example</header>
+      <Button onClick={toggleDrawer()}>{state.isDrawerOpen ? "open": "closed"}</Button>
+      <Flow />
+      <Drawer
+        anchor="right"
+        open={state.isDrawerOpen}
+        onClose={toggleDrawer()}
+      >
+
+        <h1>Drawer</h1>
+      </Drawer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Flow/index.tsx
@@ -0,0 +1,87 @@
+import { useCallback } from 'react';
+import ReactFlow, {
+  Node,
+  useNodesState,
+  useEdgesState,
+  addEdge,
+  Connection,
+  Edge,
+} from 'reactflow';
+
+import CustomNode from './CustomNode';
+
+// this is important! You need to import the styles from the lib to make it work
+import 'reactflow/dist/style.css';
+
+import './Flow.css';
+
+const nodeTypes = {
+  custom: CustomNode,
+};
+
+const initialNodes: Node[] = [
+  {
+    id: '1',
+    type: 'input',
+    data: { label: 'Node 1' },
+    position: { x: 250, y: 5 },
+  },
+  {
+    id: '2',
+    data: { label: 'Node 2' },
+    position: { x: 100, y: 100 },
+  },
+  {
+    id: '3',
+    data: { label: 'Node 3' },
+    position: { x: 400, y: 100 },
+  },
+  {
+    id: '4',
+    data: { label: 'Node 4' },
+    position: { x: 450, y: 250 },
+    type: 'custom',
+  },
+  {
+    id: '5',
+    data: { label: 'Node 5' },
+    position: { x: 200, y: 200 },
+  },
+  {
+    id: '6',
+    data: { label: 'Node 6' },
+    position: { x: 300, y: 300 },
+  },
+];
+
+const initialEdges: Edge[] = [
+  { id: 'e1-2', source: '1', target: '2', label: 'edge label' },
+  { id: 'e1-3', source: '1', target: '3', animated: true },
+  { id: 'e3-5', source: '3', target: '5', animated: true, label: 'another edge label' },
+  { id: 'e3-6', source: '3', target: '6' },
+];
+
+function Flow() {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  const onConnect = useCallback(
+    (params: Connection | Edge) => setEdges((eds) => addEdge(params, eds)),
+    [setEdges]
+  );
+
+  return (
+    <div className="Flow">
+      <ReactFlow
+        nodes={nodes}
+        onNodesChange={onNodesChange}
+        edges={edges}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        fitView
+        nodeTypes={nodeTypes}
+      />
+    </div>
+  );
+}
+
+export default Flow;
